4a5500e98061f1742188c73f9d7f9ff100ef2223
Jaskeerat Singh <jaskeerat10000@gmail.com>
2021-05-20 02:15:28 +0530

    done with creating a model , schema and adding data to the collections using object

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const FoodModel = require('./models/Food')
+
 const app = express();
 
 app.use(express.json());
 
-mongoose.connect()
+
+app.get('/' , async (req,res) => {
+    const food = new FoodModel({foodName: "Rajma" , daySinceIAte: 60});
+
+    try{
+        await food.save();
+    }
+    catch(err){
+        console.log(err);
+    }
+});
+
+mongoose.connect('mongodb+srv://crudapp:crudapp@crud.z1gcd.mongodb.net/food?retryWrites=true&w=majority' , {
+    useNewUrlParser: true,
+});
 
 app.listen(5000, () => {
     console.log("Server running on port 5000 ");
